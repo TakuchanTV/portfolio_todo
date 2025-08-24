7d9940c54ecbe918afe515c9f677fb9544bf5de5
Taku <fk.53rk2@icloud.com>
2025-08-24 12:08:10 +0900

    vercelに自動デプロイされるのかを確認するためにテストフォルダーを作った

---
+++ src/test/test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./test.module.css"
+
+export const Test = () => {
+    return (
+        <div className={styles.div}>
+           <p className={styles.span}>この画面はテスト画面です。ご注意ください</p> 
+        </div>
+    )
+}
