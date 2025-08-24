8bfa3cffc3ecfbad1ed7b5cb7c04fbe5c609b250
Taku <fk.53rk2@icloud.com>
2025-08-24 12:14:42 +0900

    デプロイが正常に作動しているか確認する

--- src/test/test.tsx
+++ src/test/test.tsx
@@ -5,6 +5,7 @@ export const Test = () => {
     return (
         <div className={styles.div}>
            <p className={styles.span}>この画面はテスト画面です。ご注意ください</p> 
+           <p className={styles.span}>この行が最新のデプロイに反映されているか確認します</p> 
         </div>
     )
 }
