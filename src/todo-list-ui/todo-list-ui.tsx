1bda0db80bcbd992818bc5210f64f187719264ef
Taku <fk.53rk2@icloud.com>
2025-08-24 11:31:48 +0900

    Todoリストの見た目を作るtsxファイルを作った

---
+++ src/todo-list-ui/todo-list-ui.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "./todo-list-ui.module.css"
+
+export const Todo_List_UI = () => {
+  return (
+    <div className={styles.div}>todo-list-ui</div>
+  )
+}
