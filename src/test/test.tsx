7758dcbd7184bdee2a7548727fc055b16afd071e
Taku <fk.53rk2@icloud.com>
2025-08-25 12:20:55 +0900

    vercelに自動デプロイができているか確認します

--- src/test/test.tsx
+++ src/test/test.tsx
@@ -5,7 +5,10 @@ export const Test = () => {
     return (
         <div className={styles.div}>
            <p className={styles.span}>この画面はテスト画面です。ご注意ください</p> 
-           <p className={styles.span}>この行が最新のデプロイに反映されているか確認します</p> 
+           <p className={styles.span}>この行が最新のデプロイに反映されているか確認します</p>
+           <div>
+            <p className={styles.test}>この文章は8/25に更新されたものです</p>
+           </div> 
         </div>
     )
 }
