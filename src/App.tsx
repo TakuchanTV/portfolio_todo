ede32e8133aed1f1ba8dc25f79390087a3240626
Taku <fk.53rk2@icloud.com>
2025-08-28 07:16:31 +0900

    App.tsxからTestコンポーネントを削除した

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Test } from './test/test';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -19,8 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
-      <Test />
+      </header>
     </div>
   );
 }
