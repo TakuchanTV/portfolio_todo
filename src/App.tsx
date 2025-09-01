9146e5498b6fe8f87c1d93c8b249aa37aa347705
Taku <fk.53rk2@icloud.com>
2025-09-01 22:00:38 +0900

    元に書かれていたコードをコメントアウトし、Todo_List_UIコンポーネントを追加した

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,22 @@ import { Todo_List_UI } from './todo-list-ui/todo-list-ui';
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
     <Todo_List_UI />
   );
 }
